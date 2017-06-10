9597a12b92eec7814d7c303b539dcc7ed7538c6e
Sébastien Lemieux <sebas.lemieux@gmail.com>
2017-06-10 09:22:15 -0400

    Added sidebar Able to add and remove characters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,52 +11,24 @@ export class AppComponent implements OnInit {
   private characters : Character[] = [];
   @ViewChild('svg') svg : ElementRef;
   private selectedElement: Character = null;
+  private selectedIndex: number;
 
   ngOnInit(){
-    this.characters.push(new Character('Steeve',150,100));
-    this.characters.push(new Character('Bob',75,75));
-    this.characters.push(new Character('Bob',50,50));
-    this.characters.push(new Character('Bob',25,25));
-    this.characters.push(new Character('Bob',100,100));
-    this.characters.push(new Character('Bob',50,75));
-    this.characters.push(new Character('Bob',75,50));
-    this.characters.push(new Character('Bob',25,75));
-    this.characters.push(new Character('Bob',75,25));
-    this.characters.push(new Character('Bob',100,75));
-    this.characters.push(new Character('Bob',75,100));
-    this.characters.push(new Character('Bob',125,75));
-    this.characters.push(new Character('Bob',75,125));
-    this.characters.push(new Character('Bob',150,75));
-    this.characters.push(new Character('Bob',75,150));
-    this.characters.push(new Character('Bob',175,75));
-    this.characters.push(new Character('Bob',75,175));
-    this.characters.push(new Character('Bob',200,75));
-    this.characters.push(new Character('Bob',75,200));
-    this.characters.push(new Character('Bob',225,75));
-    this.characters.push(new Character('Bob',75,225));
-    this.characters.push(new Character('Bob',250,75));
-    this.characters.push(new Character('Bob',75,250));
-    this.characters.push(new Character('Bob',275,75));
-    this.characters.push(new Character('Bob',75,275));
-    this.characters.push(new Character('Bob',300,75));
-    this.characters.push(new Character('Bob',75,300));
-    this.characters.push(new Character('Bob',325,75));
-    this.characters.push(new Character('Bob',75,325));
-    this.characters.push(new Character('Bob',350,75));
-    this.characters.push(new Character('Bob',375,350));
-    this.characters.push(new Character('Bob',75,375));
-    this.characters.push(new Character('Bob',400,75));
-    this.characters.push(new Character('Bob',75,400));
+
   }
 
   selectElement(id){
     this.selectedElement = this.characters[id];
+    this.selectedIndex = id;
   }
 
   mouseUp(){
     if(this.selectedElement!=null){
       this.selectedElement.x = Math.floor(this.selectedElement.x/56)*56+28;
       this.selectedElement.y = Math.floor(this.selectedElement.y/56)*56+28;
+      if(this.selectedElement.x < 56 && this.selectedElement.y < 56){
+        this.characters.splice(this.selectedIndex,1);
+      }
       this.selectedElement = null;
     }
   }
@@ -66,5 +38,9 @@ export class AppComponent implements OnInit {
       this.selectedElement.x = event.offsetX;
       this.selectedElement.y = event.offsetY;
     }
+  }
+
+  addChar(name){
+    this.characters.push(new Character(name,26,26));
   }
 }
